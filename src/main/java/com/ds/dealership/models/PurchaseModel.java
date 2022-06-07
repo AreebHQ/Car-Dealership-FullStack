52bbbb8dc686c58049b2207126ea8e727311ae6e
Areeb Hussain <69098189+AreebHQ@users.noreply.github.com>
2022-06-06 23:58:21 -0400

    angularV1

---
+++ src/main/java/com/ds/dealership/models/PurchaseModel.java
@@ -0,0 +1,95 @@
+package com.ds.dealership.models;
+
+
+public class PurchaseModel {
+
+    private String name;
+
+    private String email;
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String zip;
+
+    private int price;
+
+    private String purchaseType;
+
+    private String vehicleId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getPurchaseType() {
+        return purchaseType;
+    }
+
+    public void setPurchaseType(String purchaseType) {
+        this.purchaseType = purchaseType;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+}
